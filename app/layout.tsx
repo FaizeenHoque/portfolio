24036c7e07be59a5c307f966c516581f215710b9
Faizeen Hoque <haquefaizeen11@gmail.com>
2026-02-04 05:18:57 +0600

    add a preview image for links

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,8 +8,32 @@ const pixelifySans = Pixelify_Sans({
 });
 
 export const metadata: Metadata = {
-  title: "Faizeen Hoque - Portfolio",
-  description: "Faizeen Hoque's Portfolio Website",
+  title: "Faizeen Hoque | Portfolio",
+  description:
+    "A collection of hardware and software creations by Faizeen Hoque",
+  openGraph: {
+    title: "Faizeen Hoque | Portfolio",
+    description:
+      "A collection of hardware and software creations by Faizeen Hoque",
+    url: "https://faizeenhoque.xyz",
+    siteName: "Faizeen Hoque Portfolio",
+    images: [
+      {
+        url: "https://faizeenhoque.xyz/preview.png",
+        width: 1200,
+        height: 630,
+        alt: "Faizeen Hoque Portfolio Preview",
+      },
+    ],
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Faizeen Hoque | Portfolio",
+    description:
+      "A collection of hardware and software creations by Faizeen Hoque",
+    images: ["https://faizeenhoque.xyz/preview.png"],
+  },
 };
 
 export default function RootLayout({
